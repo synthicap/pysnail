ecfa58770b4fee1b58b710e77883083e6af44feb
synthicap <wdart1995@gmail.com>
2019-02-14 00:39:02 +0300

    2

--- crystal.py
+++ crystal.py
@@ -1,51 +1,50 @@
-inv = {0:0, 1:5, 2:4, 3:3, 4:2, 5:1}
-
+inv = [0, 5, 4, 3, 2, 1]
+axi = [3, 4, 5, 0, 1, 2]
 ext = [1, 1, 0, 0, 0, 1]
 
 def inv_pos(pos):
     return inv[pos[0]], inv[pos[1]]
 
-def track_to_crystal(track0, vec, crystal, pos_):
-    pos = (vec + pos_[0]) % 6
-    track = track0 + [(pos, crystal.r)]
-    if ext[pos]:
-        track.append((vec1, crystal.d))
-    return track, (vec + pos_[1]) % 6
+def rotate(x, y):
+    return (x + y) % 6;
 
-def track_to_module(track0, vec, crystal, pos):
-    pos = (vec + pos) % 6
-    track = track0 + [(pos, crystal.r)]
+def cur_pos_abs(pos_abs, ang, element, pos):
+    pos_abs = pos_abs.copy()
+    pos_abs[rotate(ang, pos)] += element.r
     if ext[pos]:
-        track.append((vec1, crystal.d))
-    return track
+        pos_abs[ang] += element.d
+    return pos_abs
 
-class Crystal:
-    def __init__(self, r, d, dub):
+class Element:
+    def __init__(self, r, d, dub = False, module = {}, elements = {}):
         self.r = r
         self.d = d
         self.dub = dub
-        self.modules = {}
-        self.crystals = {}
+        self.modules = modules
+        self.elements = elements
 
-    def add_crystal(self, crystal, *args):
+    def add_element(self, element, *args):
         args = (arg if arg is tuple else arg, arg for arg in args)
-        self.crystals.fromkeys(args, crystal)
+        self.elements.fromkeys(args, element)
         if dub:
             args = map(inv_pos, args)
-            self.crystals.fromkeys(args, crystal.inv_copy())
+            self.elements.fromkeys(args, element.inv_copy())
 
     def add_module(self, module, *args):
         self.modules.fromkeys(args, module)
 
     def inv_copy(self):
-        cp = Crystal(self.r, self.d, self.dub)
-        cp.modules = {inv[k], v for k, v in self.modules}
-        cp.crystals = {inv_pos(k), v.inv_copy() for k, v in self.crystals}
-        return cp
+        return Element(self.r, self.d, self.dub, 
+                {inv[k], v for k, v in self.modules},
+                {inv_pos(k), v.inv_copy() for k, v in self.elements})
 
-    def crystalis_abs(self, track0, vec):
-        return [track_to_crystal(track0, vec, self, k) + tuple(v) for k, v in self.crystals]
+    def elements_abs(self, pos_abs, ang):
+        return [(cur_pos_abs(pos_abs, ang, self, k[0]), rotate(k[1], ang), v) 
+                for k, v in self.elements]
 
-    def modules_abs(self, track0, vec):
-        return [track_to_module(track0, vec, self, k) + tuple(v) for k, v in self.crystals]
+    def modules_abs(self, pos_abs, ang):
+        return [(cur_pos_abs(pos_abs, ang, self, k), v) for k, v in self.modules]
+
+    def raw_nodes(self, pos_abs, ang):
+        
 
